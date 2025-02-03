21451c4d3566326f03fc7c86d3ddfb4eccb01da1
Jordynj917 <jojojones10@icloud.com>
2025-02-03 14:59:35 -0500

    Mini Project 2

--- startbootstrap-shop-homepage-gh-pages/js/scripts.js
+++ startbootstrap-shop-homepage-gh-pages/js/scripts.js
@@ -1,36 +1,31 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
 
 const products = [
-    {id: 1, title: , price: 15.99},
-    {id: 2, title: , price: 42.75},
-    {id: 3, title: , price: 69.99},
-    {id: 4, title: , price: 35.00},
-    {id: 5, title: , price: 34.99},
-    {id: 6, title: , price: 45.00},
-    {id: 7, title: , price: 18.00},
-    {id: 8, title: , price: 70.00},
-    {id: 9, title: , price: 66.00},
-    {id: 10, title: , price: 50.00},
-    {id: 11, title: , price: 55.00},
-    {id: 12, title: , price: 55.00},
-    {id: 13, title: , price: 39.99},
-    {id: 14, title: , price: 58.75},
-    {id: 15, title: , price: 19.99},
-    {id: 16, title: , price: 20.00}
+    {id: 1, title:"#Red #Short #Round" , price: 15.99, category:"Short"},
+    {id: 2, title:"#Red #Medium #Coffin #Floral" , price: 42.75, category: "Medium"},
+    {id: 3, title:"#Red #Long #Stilleto #Spooky" , price: 69.99, category: "Long"},
+    {id: 4, title:"#Red #Square #Long #Winter" , price: 35.00, category: "Long"},
+    {id: 5, title:"#Green #Short #Square" , price: 34.99, category: "Short"},
+    {id: 6, title:"#Green #Long #Coffin #Floral" , price: 45.00, category: "Long"},
+    {id: 7, title:"#Green #Medium #Square #Winter" , price: 18.00, category: "Medium"},
+    {id: 8, title:"#Green #Square #XL" , price: 70.00, category: "XL"},
+    {id: 9, title:"#Orange #Long #Coffin #Fall" , price: 66.00, category: "Long"},
+    {id: 10, title:"#Orange #Short #Square #Spooky" , price: 50.00, category: "Short"},
+    {id: 11, title:"#Orange #Medium #Floral #Square" , price: 55.00, category: "Medium"},
+    {id: 12, title:"#Orange #XL #Stilleto" , price: 55.00, category: "XL"},
+    {id: 13, title:"#Pink #Short #Square #Floral" , price: 39.99, category: "Short"},
+    {id: 14, title:"#Pink #Medium #Square #Spooky " , price: 58.75, category: "Long"},
+    {id: 15, title:"#Pink #Coffin #Long #Winter" , price: 19.99, category: "Long"},
+    {id: 16, title:"#Pink #Short #Round" , price: 20.00, category: "Short"}
 ];
 
 
 let categories = new Map();
 let allProducts;
 
-fetch example
-fetch('https://fakestoreapi.com/products')
-    .then(response => response.json())
-    .then(json => { allProducts = json; loadProducts(json); loadFilterOptions(); })
+// fetch example
+// fetch('/index1.html')
+//     .then(response => response.json())
+//     .then(json => { allProducts = json; loadProducts(json); loadFilterOptions(); })
 
 
     function loadProducts(products) {
@@ -47,40 +42,40 @@ fetch('https://fakestoreapi.com/products')
     }
 
     
-    function addProduct(item) {
+    // function addProduct(item) {
 
-        const template = document.getElementById("card-template").content.cloneNode(true);
-        template.querySelector('.card-title').innerText = item.title;
-        template.querySelector('.card-header').innerHTML = getCategoryIcon(item.category) + item.category;
-        template.querySelector('.card-subtitle').innerText = '$' + item.price;
-        template.querySelector('.card-img-top').src = item.image;
-        template.querySelector('.card-img-top').alt = item.title;
-        template.querySelector('.card-text').innerText = item.description.substring(0, 50) + '...';
-        template.querySelector('.card').className = 'card '+ categories.get(item.category);
-        template.querySelector('.card').id = 'product' + item.id;
-        template.querySelector('.btn').addEventListener('click', (e) => expandText(e, item.id, item.description));
-        document.querySelector('#product-list').appendChild(template);
-    }
+    //     const template = document.getElementById("card-template").content.cloneNode(true);
+    //     template.querySelector('.card-title').innerText = item.title;
+    //     template.querySelector('.card-header').innerHTML = getCategoryIcon(item.category) + item.category;
+    //     template.querySelector('.card-subtitle').innerText = '$' + item.price;
+    //     template.querySelector('.card-img-top').src = item.image;
+    //     template.querySelector('.card-img-top').alt = item.title;
+    //     template.querySelector('.card-text').innerText = item.description.substring(0, 50) + '...';
+    //     template.querySelector('.card').className = 'card '+ categories.get(item.category);
+    //     template.querySelector('.card').id = 'product' + item.id;
+    //     template.querySelector('.btn').addEventListener('click', (e) => expandText(e, item.id, item.description));
+    //     document.querySelector('#product-list').appendChild(template);
+    // }
 
     
-    function getCategoryIcon(cat) {
-        switch (cat.toLowerCase()) {
-            case "men's clothing": return '<i class="fa-solid fa-shirt"></i> ';
-            case "women's clothing": return '<i class="fa-solid fa-person-dress"></i> ';
-            case "jewelery": return '<i class="fa-regular fa-gem"></i> ';
-            case "electronics": return '<i class="fa-solid fa-tv"></i> ';
-        }
+    // function getCategoryIcon(cat) {
+    //     switch (cat.toLowerCase()) {
+    //         case "Short": return '<i class="fa-solid fa-shirt"></i> ';
+    //         case "Medium": return '<i class="fa-solid fa-person-dress"></i> ';
+    //         case "Long": return '<i class="fa-regular fa-gem"></i> ';
+    //         case "XL": return '<i class="fa-solid fa-tv"></i> ';
+    //     }
         
-        return '<i class="fa-brands fa-shirtsinbulk"></i> ';
-    }
+    //     return '<i class="fa-brands fa-shirtsinbulk"></i> ';
+    // }
 
     
-    function loadFilterOptions() {
-        let filterSelect = document.getElementById('category_filter')
-        categories.forEach((slug, cat) => {
-            filterSelect.innerHTML += `<option value="${cat}">${cat}</option>`;
-        });
-    }
+    // function loadFilterOptions() {
+    //     let filterSelect = document.getElementById('category_filter')
+    //     categories.forEach((slug, cat) => {
+    //         filterSelect.innerHTML += `<option value="${cat}">${cat}</option>`;
+    //     });
+    // }
 
    
     function filterProducts(e) {
@@ -92,7 +87,7 @@ fetch('https://fakestoreapi.com/products')
 
     function sortProducts(e) {
         let selectedOrder = e.target.value;
-        let sortedProducts = [...allProducts];
+        let sortedProducts = [...products];
 
         switch (selectedOrder) {
             case 'price_lohi': sortedProducts.sort((p1, p2) => p1.price - p2.price); break;
